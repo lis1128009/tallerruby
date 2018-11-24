354837b2a0f3ce1083a4fc611f1a0a855ead57b9
liseth <liseth.cuervo@correounivalle.edu.co>
2018-11-24 22:42:45 +0000

    Mi Primer commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "600x600#" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  has_many :comments, dependent: :destroy
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :posts do  
+  resources :comments
+end
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  root 'posts#index'
+get 'about' => 'pages#about'
+end
